e94b7039736fa1c4b7e09b63cbabb81f0baf16ce
Yuhe Ma <yuhem@andrew.cmu.edu>
2025-06-10 02:09:10 -0400

    Resolved to get my driver's license

---
+++ 25-m6/10.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "What are my plans for the summer? No matter what I need to get my driver's license first\n"
+    return 0;
+}
--- 25-m6/7.cpp
+++ 25-m6/7.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
 int main() {
-    std::cout << "I passed the 2nd exam for my driver's lesson!\n The 3rd one is scheduled on 6/16.\n";
+    std::cout << "I passed the 2nd exam for my driver's licence!\n The 3rd one is scheduled on 6/16.\n";
     return 0;
 }
