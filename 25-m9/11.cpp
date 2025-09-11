0593d146b47411a004f5a3767b2ef484622f6172
Yuhe Ma <yuhem@andrew.cmu.edu>
2025-09-11 10:22:02 -0400

    I learned a lot from being scammed by a low life

---
+++ 25-m9/11.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Study terms for network class.\n";
+    std::cout << "Finish Linear Algebra Series.\n";
+    std::cout << "Dynamic Programming x 1h.\n";
+    std::cout << "Silksong x 1h.\n";
+    std::cout << "Study channels in go.\n";
+    std::cout << "Meditate a/b Christine.\n";
+    return EXIT_SUCCESS;
+}
