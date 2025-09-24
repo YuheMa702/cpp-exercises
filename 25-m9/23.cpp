097e3309021dad5c873e0cce0c46c701f6ced853
Yuhe Ma <yuhem@andrew.cmu.edu>
2025-09-23 21:22:05 -0400

    dopamine loading

---
+++ 25-m9/23.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+
+int main() {
+    std::cout << "I learned some interesting self-control hack called dopamine loading...\n";
+    std::cout << "Basically it means doing the hardest things first in the morning.\n";
+    return EXIT_SUCCESS;
+}
