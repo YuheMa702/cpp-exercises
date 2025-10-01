675ea6e2f17f17ee001c756394f58fc3c923f72f
Yuhe Ma <yuhem@andrew.cmu.edu>
2025-09-30 22:48:36 -0400

    Anonymous love note

---
+++ 25-m9/30.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+    std::cout << "I put an anonymous love note into her backpack. Now I am so scared.\n";
+    std::cout << "I even went to the library tonight as I wanted to hide from her.\n";
+    return EXIT_SUCCESS;
+}
