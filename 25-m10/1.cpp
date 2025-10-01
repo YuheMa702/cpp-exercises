008975c7e28c91a4572eae6bc5bb09ae01c81b50
Yuhe Ma <yuhem@andrew.cmu.edu>
2025-10-01 10:24:53 -0400

    Building Math foundations

---
+++ 25-m10/1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+    std::cout << "A great day starts from planning! \n";
+    /*
+    Review Probabilities (combination vs permutation, poisson process)
+    ALOHA and Convolutional Encoding Practice
+    Database Review 4 slides + join algorithm if there is time
+    Linear Algebra Practice: Gaussian Elimination * 3 Eigen * 3
+    Review Calculus (Big Theorem + Taylor Series)
+    DSA (Data Structures & Algorithms Review): Search + Pathfinding in 1D, trees, and graphs
+    Let's focus on building foundations this semester!
+    */
+   return EXIT_SUCCESS;
+}
