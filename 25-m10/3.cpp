85475ec774c44feecdf23be2ba1db9f7e6144e87
Yuhe Ma <yuhem@andrew.cmu.edu>
2025-10-03 22:33:06 -0400

    jealous of a stupid guy from her program!

---
+++ 25-m10/3.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Dined at BAO with EU, studied together at TCS but I really do "\
+    "not like the other dude in the classroom who keeps bugging EU... He did not go to "\
+    "classes and asked EU for notes and asked her to help him with problems. Although "\
+    "he was being very polite, I still does not like him! Okay I admit I was just "\
+    "being jealousy and I am mad at myself for being jealous of that dude.\n";
+    return EXIT_SUCCESS;
+}
