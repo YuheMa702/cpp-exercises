5561486e45e1584c39a7367d3e53ebfcd0a6bc98
Yuhe Ma <yuhem@andrew.cmu.edu>
2025-10-04 15:59:02 -0400

    Need to review for my distributed systems class

---
+++ 25-m10/4.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "I feel so lost. I need to do something productive. Such as reviewing for my distributed systems class.\n";
+    return EXIT_SUCCESS;
+}
