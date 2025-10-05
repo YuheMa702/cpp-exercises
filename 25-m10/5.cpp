1995ba4dfb30d1b019cc080a7a7ab7d70f2c48fe
Yuhe Ma <yuhem@andrew.cmu.edu>
2025-10-05 09:00:41 -0400

    If only she also likes me as much...

---
+++ 25-m10/5.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+
+int main() {
+    std::cout << "How much I wish that I can occupy more of her heart.\n";
+    /*
+    Distributed Videos for 1 hours min
+    Review Slides for one lecture
+    Ask her if she's gonna go to Costco with me.
+    Do the project report in the afternoon and tell my teammate before 12a.m.
+    */
+    return EXIT_SUCCESS;
+}
