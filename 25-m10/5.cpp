1019a823fb43ca09adb42ad556c2e977ffafd59e
Yuhe Ma <yuhem@andrew.cmu.edu>
2025-10-06 13:02:26 -0400

    I wanna have a girlfriend

--- 25-m10/5.cpp
+++ 25-m10/5.cpp
@@ -4,9 +4,9 @@
 int main() {
     std::cout << "How much I wish that I can occupy more of her heart.\n";
     /*
-    Distributed Videos for 1 hours min
-    Review Slides for one lecture
-    Ask her if she's gonna go to Costco with me.
+    Distributed Videos for 1 hours min (Done)
+    Review Slides for one lecture 
+    Ask her if she's gonna go to Costco with me. (Done)
     Do the project report in the afternoon and tell my teammate before 12a.m.
     */
     return EXIT_SUCCESS;
---
+++ 25-m10/6.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Why is it sooo hard to find a girlfriend... I know I should be focusing on my exams but\n";
+    return EXIT_SUCCESS;
+}
