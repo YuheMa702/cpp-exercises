bbbb3880351ba1fe731ace063145520d1655f336
Yuhe Ma <yuhem@andrew.cmu.edu>
2025-10-07 17:56:32 -0400

    life is hard but so am I ...

---
+++ 25-m10/7.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    std::cout << "I am the self-disciplined, smart, and ambitious person I am.\n";
+    /*
+    Just had my computer network exam, oops did not finish it.
+    */
+    return EXIT_SUCCESS;
+}
