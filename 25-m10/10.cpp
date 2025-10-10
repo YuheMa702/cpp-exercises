59198fba06425f7dfd30abf20a07af8735ebfabe
Yuhe Ma <yuhem@andrew.cmu.edu>
2025-10-10 10:50:37 -0400

    Alianna is here YAY

---
+++ 25-m10/10.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+
+int main() {
+    std::cout << "I have spent so much efforts studying for the network exam, yet still only got 77 lol.\n";
+    std::cout << "Good news Alianna is here with me it's so nostalgic just chatting with her in person.\n";
+    std::cout << "I told her she used to be my crush and my current crush is EU lol.\n";
+    std::cout << "We are gonna have such a good time in the next few days!\n";
+    return EXIT_SUCCESS;
+}
