4a341243606c976bdedc0857d5d4ddf862fe0e78
Yuhe Ma <yuhem@andrew.cmu.edu>
2025-10-11 10:51:57 -0400

    I must seize the opportunity

---
+++ 25-m10/11.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    std::cout << "I learned so much lessons about romantic relationships after chatting with Alianna.\n";
+    std::cout << "Yes I must seize the opportunity, otherwise I will regret for life.\n";
+    std::cout << "Even if I get rejected, even if she and I can only be together for a few years.\n";
+    return EXIT_SUCCESS;
+}
