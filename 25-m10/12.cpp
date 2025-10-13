93fec3acd9e086e745df8a06f19f166b5610c6f2
Yuhe Ma <yuhem@andrew.cmu.edu>
2025-10-12 23:32:06 -0400

    I am a fool in love

---
+++ 25-m10/12.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+
+int main() {
+    std::cout << "Love makes one blind, makes one stupid, makes one emotional!!!\n";
+    std::cout << "Can't believe Alianna missed her flight...\n";
+    return EXIT_SUCCESS;
+}
