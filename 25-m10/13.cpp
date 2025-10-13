cefc224770ea7b42027c8e259e741631f8aafba6
Yuhe Ma <yuhem@andrew.cmu.edu>
2025-10-13 10:32:26 -0400

    Columbus Day and Mail card

---
+++ 25-m10/13.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Happy Columbus Day.\nI still feel like an idiot in love...\n";
+
+    /*
+    Today I need to mail Alianna's Card***
+    Go to Costco with my ECE friend amd bring a hotdog back for her.
+    Do leetcode 5 problems
+    */
+   return EXIT_SUCCESS;
+}
