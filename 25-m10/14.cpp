f72b185098c5763a8e8d02dec512804a59be166c
Yuhe Ma <yuhem@andrew.cmu.edu>
2025-10-14 23:40:33 -0400

    We went to the botanical garden + good night function

--- 25-m10/14.cpp
+++ 25-m10/14.cpp
@@ -12,5 +12,19 @@ int main() {
         std::cout << c;
     }
     std::cout << "我爱你\n";
+    /*
+    Damn my heart is beating so fast I cannot breath!!!!
+    What have I done??? Did I just wrote a goofy never ending recursive good night function
+    and send it to her?! Would it be too obvious?? I even added a blue heart after
+    stack overflow comment I feel so stupid I don't know how she is gonna react to it.
+    She probably has not seen it yet because her notifications might be muted!
+    I feel like there is no turning back, that was toooo obvious!!!!
+    That was very close to confessing my love to her!!!!
+    Damn she says hahahahahaha, what does that mean!????
+    Ok she probably did not see the hidden meanings behind the function.
+    Probably or did she? I don't know! It's too hard to guess!
+    OK I think she thought it was just a creative good night message.
+    Anyway that was a HUGE step for me!!!!
+    */
     return EXIT_SUCCESS;
 }
