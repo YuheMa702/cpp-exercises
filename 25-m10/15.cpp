85a8ee054ab27ab1dd6d5c29c995e0f127929e9a
Yuhe Ma <yuhem@andrew.cmu.edu>
2025-10-15 09:28:51 -0400

    cleared my fridge

---
+++ 25-m10/15.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Great habbit to go to bed early and get up early!\n";
+    std::cout << "I feel that my life is on track now.\n";
+    /*
+    1. 5 brain exercises
+    2. Database B+ tree video plus query execution lectures
+    3. C++ data structures and STLs: vector, map, set, unordered_map, stack, etc.
+    4. Leetcode hashmap/set: 2 problems
+    5. Computer networks project 2 handout 
+    6. Distributed Systems project 1 Part B handout
+    7. What physics lectures for fun
+    */
+   return EXIT_SUCCESS;
+}
