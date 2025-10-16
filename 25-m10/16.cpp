5e4f37a3e6a53d175837f1ecf10ac2e16d8d45ba
Yuhe Ma <yuhem@andrew.cmu.edu>
2025-10-16 17:47:43 -0400

    Study with her in the library

---
+++ 25-m10/16.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    std::cout << "I spend the entire afternoon studying in Sorrels with her lol.\n";
+    /*
+    So much to learn for database systems!
+    I still gotta preview my computer networks and distributed systems class
+    Need to master go for that course, yet I am still struggling with c++....
+    Again, do at least 3 leetcode problems, let's do queues
+    */
+   return EXIT_SUCCESS;
+}
