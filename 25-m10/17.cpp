ff3a7560e9d16858dc4c5a446c19555a1c005a98
Yuhe Ma <yuhem@andrew.cmu.edu>
2025-10-17 00:10:29 -0400

    friday plan ahead 10.17

---
+++ 25-m10/17.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+
+int main() {
+    /*
+    Morning: brain exercise, 1D DP problems x 5 + tutorials, greedy, DFS BFS UCS
+    Afternoon: databases log + recovery, project proposals draft + dev ennv set up
+    Evening: distributed + computer networks review 1 hour each
+    Do something with her
+    */
+
+    return EXIT_SUCCESS;
+}
