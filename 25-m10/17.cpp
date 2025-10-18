991d8c382bcbd1b70d5f542063bd81c173aff74b
Yuhe Ma <yuhem@andrew.cmu.edu>
2025-10-17 23:37:49 -0400

    happy friday

--- 25-m10/17.cpp
+++ 25-m10/17.cpp
@@ -1,5 +1,20 @@
 #include <iostream>
 
+void commonDPPatterns() {
+    /* DP is recursive in nature, summing res from subproblems => res for big problem
+       Bottom up without memoization is the queen!
+       Depending on the problem, save the n positions you need to calculate curr position
+    */
+    /* Fibo */
+    /* 0/1 Knapsack (optimization problems) 2D */
+    /* Unbounded Knapsack (optimization can use an item inf times) 2D */
+    /* Longest Common Subsequence 2D */
+    /* Palindromes (start in the middle for odd len, start with 2 for even ones) */
+}
+
+
+
+
 
 int main() {
     /*
@@ -8,6 +23,9 @@ int main() {
     Evening: distributed + computer networks review 1 hour each
     Do something with her
     */
-
+    std::cout << "I think she likes me but I love her.\n";
+    std::cout << "I am so desperately in love. Our physical proximity drives me crazy.\n";
+    std::cout << "She is literally next door but feel so so far away, unreachable.\n";
+    std::cout << "I want to be pinned against a wall by her.\n";
     return EXIT_SUCCESS;
 }
