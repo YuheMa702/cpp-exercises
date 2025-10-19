f188469c673a202ecc8731afc9ccf9184e11203d
Yuhe Ma <yuhem@andrew.cmu.edu>
2025-10-19 08:58:00 -0400

    10.19.25 day plan

---
+++ 25-m10/19.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    /* 
+    I need to finish my memory hierarchy homework + draft proposal for Advanced database today.
+    Morning: Luminity, probability + markov chains reviews, database log + recovery
+    Lunch: make green beans + beef + cucumber
+    Afternoon: database proposal draft
+    Dinner: whatever
+    Evening: memory hierarchy
+    Bonus: if have time, watch 2D dynamic programming videos
+    */
+    std::cout << "Well I texted good night to her but she did not reply...\n";
+}
