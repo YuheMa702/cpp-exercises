88daf731406093273eb7b7b046ac8b0d0a9b5bff
Yuhe Ma <yuhem@andrew.cmu.edu>
2025-10-18 23:24:39 -0400

    Learned my scholar heritage

---
+++ 25-m10/18.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Went to costco with her! Had dinner with her! Today was not very productive... but"\
+    " I learned my scholar heritage of my father's side of family from my mom which makes me more motivated to "\
+    " pursue a PHD degree.\n";
+    return EXIT_SUCCESS;
+}
