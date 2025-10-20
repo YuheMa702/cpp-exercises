f908643074f1fbcb80cd19b0806b07ec131e0441
Yuhe Ma <yuhem@andrew.cmu.edu>
2025-10-20 02:19:05 -0400

    Jerry! I am so envious of you lol! I feel more motivated to study than ever

---
+++ 25-m10/20.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+    std::cout << "What have I done? I reconnected with Jerry and gifted him Stardew lol.\n";
+    /*
+    It's past 2am, I am just glad I don't have PE classes anymore.
+    However I do need to exercise regularly to maintain my fitness level
+    Today I have to finish my memory hierarchy homework and seriously study for my database exam.
+    Learn SIMD instructions!
+    Review x86 assembly!
+    Lumonity as usual when I get up.
+    Oh and hot dog as breakfast
+    */
+   return EXIT_SUCCESS;
+}
