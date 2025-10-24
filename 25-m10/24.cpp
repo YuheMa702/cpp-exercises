d221265ab20451ee69a76ee151d1df043f7bbb09
Yuhe Ma <yuhem@andrew.cmu.edu>
2025-10-24 16:02:18 -0400

    I need to be more rational and selfish from now on

---
+++ 25-m10/24.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+    std::cout << "I think I cannot be trapped by my unrequited love for her.\n";
+    /*
+    She is inside my mind constantly but apparently I am not as important to her as she is to me.
+    This creates an unfair dynamic that my mind is constantly occupied by her at the expenses of
+    my own academic performance and career development. I should focus more on my personal growth
+    instead of wasting my time fantasizing about her.
+    I should prioritize my own mental and physical health and avoid doing too much for her when
+    she even does not need or appreciate the things I have done for her.
+    */
+    std::cout << "My midterm results were okay and I should aim for higher grades from now on.\n";
+    return EXIT_SUCCESS;
+}
