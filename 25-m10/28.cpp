dc6e53806fbda9a91ecb3cfca0c89c157d2f462a
Yuhe Ma <yuhem@andrew.cmu.edu>
2025-10-28 20:59:01 -0400

    Rejected by crush

---
+++ 25-m10/28.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+    std::cout << "I was very awkward seeing her this morning. I used all my strength to suppress my laugh.\n";
+    /*
+    I had not replied to her last message yet cuz I was quite puzzled by it and don't know what to say.
+    Basically she was saying I should get myself a date since I am so romantic and knows how to please a girl.
+    Well, what does that mean? Was she trying me to push me away by suggesting that I should go after other girls
+    or was she checking if I also have flirted with other girls or was she encouraging me to confess my love for her?
+    Or she was just being nice and did not mean anything by saying them.
+    Women are so hard to read.
+    Also I joined a AI CPU design tech talk this noon and was inspired by it.
+    There are so much to learn and I feel like I know nothing.
+    */
+   // OK I can finally focus on my studies now as I was rejected by my crush
+   return EXIT_SUCCESS;
+}
