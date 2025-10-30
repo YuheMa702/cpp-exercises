3a3bb4fc5b8b4c4de86191969b3e7c4663d00bba
Yuhe Ma <yuhem@andrew.cmu.edu>
2025-10-30 18:07:03 -0400

    Girl Friend vs Girlfriend

---
+++ 25-m10/30.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    std::cout << "I think I might lose another old friend due to my orientation lol.\n";
+    // However I only want girlfriend not girl friend.
+    // Life is such a struggle, I need to work hard to stay competitive in my field of study
+    return EXIT_SUCCESS;
+
+}
