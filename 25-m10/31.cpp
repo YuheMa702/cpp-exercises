10c57ce159202fdad67cddaf0ea29efa765acfe5
Yuhe Ma <yuhem@andrew.cmu.edu>
2025-10-31 23:34:47 -0400

    I am fine

---
+++ 25-m10/31.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    // How do I overcome my fear?
+    // How can I overcome the feelings of inbalance?
+    std::cout << "There is no easy way than putting into my time and efforts.\n";
+    // Haha I think I am fine now!
+    return EXIT_SUCCESS;
+}
