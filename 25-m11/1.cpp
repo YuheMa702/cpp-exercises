4a132bf91c3c01588a3ddbd7b42fd08b88c66f15
Yuhe Ma <yuhem@andrew.cmu.edu>
2025-11-01 23:57:05 -0400

    focus on my studies

---
+++ 25-m11/1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+
+int main() {
+    std::cout << "I am such a loser, both socially and academically behind my peers.\n";
+    // I will become better by making consistent progress everyday
+    // It seems that Alianna will reject me lol, friend - 1, which is good for focusing on my studies.
+    return EXIT_SUCCESS;
+}
