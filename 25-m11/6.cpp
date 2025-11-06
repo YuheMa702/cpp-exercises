1a677d4268af34a23a3af39d78d152c2bcce7af7
Yuhe Ma <yuhem@andrew.cmu.edu>
2025-11-06 02:00:16 -0500

    identify change

---
+++ 25-m11/6.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    std::cout << "I must be more rational, be more responsible, I need to love myself first.\n";
+    // When I change my self-identity, I become someone I can only dream of in the past.
+    // What kind of woman do I want to become, I want to be intelligent, rational, responsible,
+    // beautiful, charismatic, confident, self-disciplined, resillient, strong, healthy,
+    // lucky, wealthy, famous, beloved, clean, tidy, agile, true to myself
+    return EXIT_SUCCESS;
+}
