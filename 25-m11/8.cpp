199a6c745c7c42a8f0e49bbde7435693ec3e9cd0
Yuhe Ma <yuhem@andrew.cmu.edu>
2025-11-09 18:35:52 -0500

    My body, mind, and heart...

---
+++ 25-m11/8.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    std::cout << "I met with Rebecca, a first yeat CS student from China who's bi.\n";
+    // She made me realize that I look younger than my actual age
+    // I feel even more motivated to start a skin care routine
+    // We talked a lot of socioeconomic political issues
+    // She seems to be able to think very critically
+    // She said my hands are so small that I should consider being a surgeon ???
+    return EXIT_SUCCESS;
+}
---
+++ 25-m11/9.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+    std::cout << "My body wants rest, my mind wants stimulation, my heart wants connection.\n";
+    // I feel overwhelmed
+    return EXIT_SUCCESS;
+}
