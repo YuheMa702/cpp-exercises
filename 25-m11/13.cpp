31e8b2e785b06bbfd4a23794410ae2cc30abcaa6
Yuhe Ma <yuhem@andrew.cmu.edu>
2025-11-14 00:45:17 -0500

    skincare + mini date

---
+++ 25-m11/13.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+    std::cout << "I will start a simple skincare routine from now on!\n";
+    // Tomorrow I will have a mini date with Rebecca, very excited about it!
+    return EXIT_SUCCESS;
+}
