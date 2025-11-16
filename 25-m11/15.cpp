46c3c8b1074d71c25a3be3603ddfccb4d46572f7
Yuhe Ma <yuhem@andrew.cmu.edu>
2025-11-15 23:21:11 -0500

    bold rebecca

---
+++ 25-m11/15.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Will I ever meet my soulmate? Where are you? I am not lowering my standards.\n";
+    // Damn Rebecca is such a bold/rebellious girl. Can't believe she actually would hook up with guys.
+    return EXIT_SUCCESS;
+}
