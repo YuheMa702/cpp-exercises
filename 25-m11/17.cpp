48714175d1a4658211af58e2fb673e3e6970c42b
Yuhe Ma <yuhem@andrew.cmu.edu>
2025-11-17 19:56:02 -0500

    Why dating when I should be focused on my studies?

---
+++ 25-m11/17.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    std::cout << "I need to optimize my sleep.\n";
+    /*
+    1. Sleep at exact same time and wake up at the same time everyday. (12-7:30)
+    2. Journaling or relaxing activities like meditation 1h before bedtime
+    3. No coffee after 3pm
+    4. Sleep in coldness, darkness, and quietness
+    */
+   return EXIT_SUCCESS;
+}
