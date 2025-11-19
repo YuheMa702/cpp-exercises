4ca5180ae57d3057d13f62c1a74fa0a948e36133
Yuhe Ma <yuhem@andrew.cmu.edu>
2025-11-18 23:15:49 -0500

    Done all my tasks + course registration

---
+++ 25-m11/18.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+int main() {
+    std::cout << "Why am I tired everyday?\n";
+    // I need to sleep more, or sleep more efficiently
+    /*
+    1. Finish computer network programming assignment (TOP PRIORITY)
+    2. Finish DB required readings
+    3. Finish DB class notes and open a pull request (TOP PRIORITY)
+    DONE!!!! Now I can have some fun (can't believe I actually finished my tasks for today)
+    */
+
+   return EXIT_SUCCESS;
+}
