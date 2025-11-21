d45da8641bf84e2a77f98c1ec6af84015a8175dd
Yuhe Ma <yuhem@andrew.cmu.edu>
2025-11-21 16:17:51 -0500

    Verified schedule for S26 and got a new pair of earbuds from Costco

---
+++ 25-m11/20.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Met with Tamal and verified my schedule for next sememster.\n";
+    /*
+    I wonder if I can really continue my research with Prof. Patel but I will try
+    He's very busy being the department head which I learned from Tamal
+    Anyways, Chris is such a helpful and nice TA
+    */
+   return EXIT_SUCCESS;
+}
---
+++ 25-m11/21.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    std::cout << "I think I am recovering from my dry coughs which.\n";
+    // Went to costco and purchased a pair of new bone-conduction earbuds
+    /* 
+    1. Finish my computer network project
+    2. Look into my distributed systems checkpoint
+    3. Priority queue problems in leetcode
+    */
+   return EXIT_SUCCESS;
+}
